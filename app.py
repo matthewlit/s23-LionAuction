1cf4207f024b8453420bc9895fc29cde5d65d717
Matthew Kelleher <Matthew.kelleher10@comcast.net>
2023-04-13 15:48:32 -0400

    Created roll specific login

--- app.py
+++ app.py
@@ -12,27 +12,53 @@ host = 'http://127.0.0.1:5000/'
 def login():
     # On login button press
     if request.method == 'POST':
+        roll = request.form['roll']
         username = request.form['username']
         password = request.form['password']
 
         # Gets hashed password from user database
         connection = sql.connect('database.db')
-        cursor = connection.execute('SELECT password FROM users WHERE email=?', (username,))
+        if roll == 'bidder':
+            cursor = connection.execute('SELECT password FROM users,bidders WHERE users.email=? AND users.email=bidders.email',
+                                        (username,))
+        elif roll == 'seller':
+            cursor = connection.execute('SELECT password FROM users,sellers WHERE users.email=? AND users.email=sellers.email',
+                                        (username,))
+        elif roll == 'helpdesk':
+            cursor = connection.execute('SELECT password FROM users,helpdesk WHERE users.email=? AND users.email=helpdesk.email',
+                                        (username,))
         result = cursor.fetchone()
 
         # Perform authentication check
         if result is not None and bcrypt.checkpw(password.encode('utf-8'), result[0]):
-            return render_template('main.html', error=None, result=username)
+            if roll == 'bidder':
+                return render_template('bidderMain.html', error=None, result=username)
+            elif roll == 'seller':
+                return render_template('sellerMain.html', error=None, result=username)
+            elif roll == 'helpdesk':
+                return render_template('helpdeskMain.html', error=None, result=username)
         else:
             return render_template('login.html', error='Invalid Login: Try Again')
 
     return render_template('login.html')
 
 
-# Main Page
-@app.route('/main', methods=['GET', 'POST'])
-def main():
-    return render_template('main.html')
+# Bidder Main Page
+@app.route('/bidderMain', methods=['GET', 'POST'])
+def bidderMain():
+    return render_template('bidderMain.html')
+
+
+# Seller Main Page
+@app.route('/sellerMain', methods=['GET', 'POST'])
+def sellerMain():
+    return render_template('sellerMain.html')
+
+
+# Help Desk Main Page
+@app.route('/helpdeskMain', methods=['GET', 'POST'])
+def helpdeskMain():
+    return render_template('helpdeskMain.html')
 
 
 if __name__ == "__main__":
